a3b8696ca97c98c6a17fff484574a931f466510c
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-23 16:43:02 +0300

    Start

---
+++ Lesson_31_C++/Soft.cpp
@@ -0,0 +1,31 @@
+#include "Soft.h"
+
+Soft::Soft()
+{
+}
+
+Soft::Soft(string name, string manufacture)
+{
+}
+
+Soft::~Soft()
+{
+}
+
+void Soft::setName(string name)
+{
+}
+
+void Soft::setManufacture(string manufacture)
+{
+}
+
+string Soft::getName() const
+{
+    return string();
+}
+
+string Soft::getManufacture() const
+{
+    return string();
+}
---
+++ Lesson_31_C++/Soft.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class Soft
+{
+protected:
+	string name;
+	string manufacture;
+public:
+	Soft();
+	Soft(string name, string manufacture);
+	virtual ~Soft();
+
+	void setName(string name);
+	void setManufacture(string manufacture);
+
+	string getName()const;
+	string getManufacture()const;
+
+	virtual void showInfo() const = 0;
+	virtual bool isAvailable() const = 0;
+
+};
+
