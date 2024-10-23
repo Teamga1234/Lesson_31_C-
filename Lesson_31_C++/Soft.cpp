48cfe687e95de8a9d94679a3abaa110a1366279f
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-23 16:46:28 +0300

    realise

--- Lesson_31_C++/Soft.cpp
+++ Lesson_31_C++/Soft.cpp
@@ -2,10 +2,14 @@
 
 Soft::Soft()
 {
+    name = "none";
+    manufacture = "none";
 }
 
 Soft::Soft(string name, string manufacture)
 {
+    this->name = name;
+    this->manufacture = manufacture;
 }
 
 Soft::~Soft()
@@ -14,18 +18,20 @@ Soft::~Soft()
 
 void Soft::setName(string name)
 {
+    this->name = name;
 }
 
 void Soft::setManufacture(string manufacture)
 {
+    this->manufacture = manufacture;
 }
 
 string Soft::getName() const
 {
-    return string();
+    return name;
 }
 
 string Soft::getManufacture() const
 {
-    return string();
+    return manufacture;
 }
