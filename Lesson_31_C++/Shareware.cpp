a0a8aeaa81b459c158a9a90b8ff11b491f0a127c
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-23 17:49:37 +0300

    Shareware cpp

--- Lesson_31_C++/Shareware.cpp
+++ Lesson_31_C++/Shareware.cpp
@@ -2,30 +2,46 @@
 
 Shareware::Shareware()
 {
+    days = 0;
+
 }
 
-Shareware::Shareware(string name, string manufacture, Date install, int days)
+Shareware::Shareware(string name, string manufacture, Date install, int days):
+    Soft( name,  manufacture)
 {
+    this->install = install;
+    this->days = days;
 }
 
 void Shareware::setInstall(Date install)
 {
+    this->install = install;
 }
 
 void Shareware::setDays(int days)
 {
+    this->days = days;
 }
 
 Date Shareware::getInstall() const
 {
-    return Date();
+    return install;
 }
 
 int Shareware::getDays() const
 {
-    return 0;
+    return days;
 }
 
 void Shareware::showInfo() const
 {
+    cout <<"Type: Shareware\n" << "Name: " << name << "\nManufacture : " << manufacture << endl;
+    cout << "Date: " << install << endl;
+    cout << "Days: " << days << endl;
+    cout << "Is available: " << (isAvailable() ? "yes" : "no") << endl;
+}
+
+bool Shareware::isAvailable() const
+{
+    return true;
 }
