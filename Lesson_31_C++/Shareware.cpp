bf0cdab21a0eda0cccacbcbae2df134b36613be4
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-23 17:43:13 +0300

    Shareware header

---
+++ Lesson_31_C++/Shareware.cpp
@@ -0,0 +1,31 @@
+#include "Shareware.h"
+
+Shareware::Shareware()
+{
+}
+
+Shareware::Shareware(string name, string manufacture, Date install, int days)
+{
+}
+
+void Shareware::setInstall(Date install)
+{
+}
+
+void Shareware::setDays(int days)
+{
+}
+
+Date Shareware::getInstall() const
+{
+    return Date();
+}
+
+int Shareware::getDays() const
+{
+    return 0;
+}
+
+void Shareware::showInfo() const
+{
+}
---
+++ Lesson_31_C++/Shareware.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Soft.h"
+#include "Date.h"
+class Shareware : public Soft
+{
+protected:
+	Date install;
+	int days;
+public:
+	Shareware();
+	Shareware(string name, string manufacture, Date install
+		, int days);
+
+	void setInstall(Date install);
+	void setDays(int days);
+
+	Date getInstall()const;
+	int getDays()const;
+	void showInfo()const override;
+
+};
+
